292b345f648adf3a417f21a461d96d3de853f096
{Your <ngadi.l.paul@gmail.com>
2023-05-31 11:58:49 +0000

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,22 +1,20 @@
-#include "main.h"
+include "main.h"
 /**
  *jack_bauer -  count down 24hrs
  *
  *Return: void.
  */
+
 void  jack_bauer(void)
 {
-int hour, minute;
-for (hour = 0; hour <= 23; hour++)
-{
-for (minute = 0; minute <= 59; minute++)
-{
-_putchar('0' + (hour / 10));
-_putchar('0' + (hour % 10));
-_putchar(':');
-_putchar('0' + (minute / 10));
-_putchar('0' + (minute % 10));
-_putchar('\n');
-}
-}
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			printf("%02d:%02d\n", hour, minute);	
+		}
+	}
+	
 }
