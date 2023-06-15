363765ad55998359400bc3513dbd0417d74b3495
{Your <ngadi.l.paul@gmail.com>
2023-06-15 21:39:32 +0000

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,18 @@
 #include <stdlib.h>
+
 /**
   * malloc_checked - allocates memory using malloc.
   * @b: size of memory to be allocated.
   *
   * Return: pointer to allocated memory.
   */
+
 void *malloc_checked(unsigned int b)
 {
-	void *p = malloc(b);
+	void *ptr = malloc(b);
 
-	if (p == NULL)
-		exit(98);
+	if (ptr == NULL)
+		_Exit(98);
 
-	return (p);
+	return (ptr);
 }
