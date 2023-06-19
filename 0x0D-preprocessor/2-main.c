55820796702b7b7086d3dda30dce568cecedbd5c
{Your <ngadi.l.paul@gmail.com>
2023-06-19 20:12:35 +0000

    Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
-*main - prints name of file.
-*
-*Return: 0.
-*/
-
+  *main - prints name of file.
+  *
+  *Return: 0.
+  */
 int main(void)
 {
-	printf("Compiled from file: %s\n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
