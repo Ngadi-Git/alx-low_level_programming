f1cb1b2846987c30c3201540b001f9922adeccd9
{Your <ngadi.l.paul@gmail.com>
2023-06-22 22:08:50 +0000

    Task 3

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,8 @@
-#include <stdarg.h>
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 /**
   *sum_them_all - returns the sum of all its parameters.
   *@n: number of arguments to be summed.
