083b1fe54751dfe297f6664431fc5cf275ffd7a5
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-07-05 16:54:55 +0000

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,7 +15,7 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-	if (x == 1)
-		return (1);
-	return (x * _pow_recursion(y--));
+	if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y--));
 
