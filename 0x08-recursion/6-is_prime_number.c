b1621c046aa3e9553477e94143e1b9a9522c1896
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-07-06 00:23:54 +0000

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ *is_divisible - checks if given number is divisible by any
+ *		 number between 2 and its square.
+ *@n: the number being checked.
+ @divisor: possible factor (divisor) of the number.
+ *
+ *Return: 0 if n is not prime
+ *	  1 if n is a prime number
+ */
+
+int is_divisible(int n, int divisor)
+{
+	if (divisor == 1)
+		return (0);
+	else if (divisor * divisor > n)
+		return (0);
+	else if (n % divisor == 0)
+		return (1);
+	else
+		return (is_divisible(n, divisor + 1));
+}
+
+/**
+ *is_prime_number - checks if number is prime or not.
+ *@n: number to be checked.
+ *
+ *Return: 1 if n is prime.
+ *0 if n is not prime.
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	else
+		return (!is_divisible(n, 2));
+}
