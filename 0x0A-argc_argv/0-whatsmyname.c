1237edc18aee37643a348151b89a05ad501240ef
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-07-08 02:54:20 +0000

    Readme

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints its own name
+ *
+ * @argc: This is the argument count
+ * @argv: This is the argument vector
+ *
+ * Return: This return to 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	(void)argc;
+	return (0);
+}
