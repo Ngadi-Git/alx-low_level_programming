0889cb23b7ca7fe2d083b7931a86e4761b79a295
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-07-12 21:10:54 +0000

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,43 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
-
 /**
- * main - Program that adds positive numbers
- * @argc: This is the argument count
- * @argv: This is the argument vector
+ *main - Program that adds positive numbers
+ *@argc: This is the argument count
+ *@argv: This is the argument vector
  *
- * Return: 0;
+ *Return: 0;
  */
-int main(int argc, char *argv[])
-{
-	int ind;
 
-	int sum = 0;
+int main(int argc, char *argv[]) {
+    int sum = 0;
+    int i;
 
-	if (argc > 1)
-	{
-		for (ind = 1; ind < argc; i++)
-		{
-			int secnd;
-			char *str;
+    if (argc == 1) {
+        printf("0\n");
+        return 0;
+    }
 
-			str = argv[ind];
-			for (secnd = 0; str[secnd] != '\0'; secnd++)
-			{
-				if (str[secnd] <= 47 || str[secnd] >= 58)
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-		}
-	}
-	for (ind = 1; ind < argc; ind++)
-	{
-		sum += atoi(argv[ind]);
-	}
-	printf("%d\n", sum);
-	return (0);
+    for (i = 1; i < argc; i++) {
+        int j;
+        for (j = 0; argv[i][j] != '\0'; j++) {
+            if (!isdigit(argv[i][j])) {
+                printf("Error\n");
+                return 1;
+            }
+        }
+        sum += atoi(argv[i]);
+    }
+
+    printf("%d\n", sum);
+    return 0;
 }
