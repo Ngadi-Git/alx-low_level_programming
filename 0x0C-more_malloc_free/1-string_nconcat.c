5bc750dd06a1ddde14ddf746c81efcf81b6aeb7b
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-07-14 21:50:20 +0000

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,54 +3,52 @@
 #include "main.h"
 
 /**
+ * strlent - calculate and return string length
+ * @string: string
+ * Return: string length
+ */
+int strlent(char *string)
+{
+	int indx;
+
+	for (indx = 0; string[indx] != '\0'; indx++)
+		;
+	return (indx);
+}
+
+/**
  * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
  * @s1: string 1
  * @s2: string 2
  * @n: n bytes to concat from string 2
  * Return: pointer to concatenated string
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	int number, len, i, j;
+	int num, len, i, j;
 
-	unsigned int length1, length2;
-
-	length1 = 0;
-	length2 = 0;
-	number = n;
+	num = n;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
-
-	if (number < 0)
+	if (num < 0)
 		return (NULL);
+	if (num >= _strlen(s2))	num = _strlen(s2);
 
-	while (s1[length1] != '\0')
-		length1++;
+	len = _strlen(s1) + num + 1; /* +1 to account for null pointer */
 
-	while (s2[length2] != '\0')
-		length2++;
-
-	if (number >= length2)
-		number = length2;
-
-	len = length1 + number + 1;
-	ptr = malloc(sizeof(*ptr) * len);
-
+	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++) /* concat */
 		ptr[i] = s1[i];
-
-	for (j = 0; j < number; j++)
+	for (j = 0; j < num; j++)
 		ptr[i + j] = s2[j];
-
 	ptr[i + j] = '\0';
+
 	return (ptr);
 }
