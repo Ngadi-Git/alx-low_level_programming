a446cb921f8c5edb148f418856b7f7e9e0a48079
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-07-17 23:58:33 +0000

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,23 +2,23 @@
 #define STRTYP
 
 /**
-  *struct dog - The struct of a dog.
-  *@name: name of the dog.
-  *@age: age of the dog.
-  *@owner: owner of the dog.
+  *struct dog - structure of a dog.
+  *@name: name of dog.
+  *@age: age of dog.
+  *@owner: owner of dog.
   */
 struct dog
 {
-	char *d_name;
-	float d_age;
-	char *d_owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 typedef struct dog dog_t;
 
-void init_dog(struct dog *d, char *d_name, float d_age, char *d_owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_t *new_dog(char *d_name, float d_age, char *d_owner);
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif /*STRTYP*/
