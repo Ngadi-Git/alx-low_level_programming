937f9a4e4240fc78aa5aa874192bed6fd2503278
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-07-19 23:56:39 +0000

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,10 @@
 /**
-  *print_name - prints a name.
+  *print_name - function that prints a name.
   *@name: pointer to the name.
   *@f: function pointer.
   *
   *Return: void.
   */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
