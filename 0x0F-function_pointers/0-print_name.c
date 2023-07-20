5995021b20809d6e77a3a520a958529965b0b429
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-07-20 01:00:56 +0000

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 
 /**
-  *print_name - function that prints a name.
-  *@name: pointer to the name.
-  *@f: function pointer.
-  *
-  *Return: void.
-  */
+ * print_name - print a name
+ * @name: the string to print
+ * @f: pointer to function
+ *
+ * Return: function to pointer to print name
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (f)
+	{
 		(*f)(name);
+	}
 }
