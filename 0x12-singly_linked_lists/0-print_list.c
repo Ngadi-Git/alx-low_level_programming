d69201997ab3c3f50f4eb0a57686594f6992c056
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-07-27 23:56:59 +0000

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,18 @@
 #include "lists.h"
-
 /**
- * print_list - prints all the elements of a list_t list.
+ * list_len - returns the number of elements in a list.
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
 
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
-	size_t node_elt;
+	size_t num_nod = 0;
 
-	node_elt = 0;
-	while (h != NULL)
+	for (; h != NULL; h = h->next)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		node_elt++;
+		num_nod++;
 	}
-	return (node_elt);
+
+	return (num_nod);
 }
