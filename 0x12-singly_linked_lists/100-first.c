9b9b1b2cab543e9ef434db58037f1bf27bfac8dd
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-07-28 00:24:03 +0000

    Task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,9 +4,9 @@
  * first - function executed before main
  * Return: no return.
  */
-void print_before_main(void) __attribute__((constructor));
 
-void print_before_main(void)
+void __attribute__ ((constructor)) first()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
