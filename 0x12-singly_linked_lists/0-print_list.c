010aab7a595a1d54a45c0a2f2fe958f1106bdd21
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-07-28 00:37:32 +0000

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,16 +8,15 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nod_elt = 0;
+	size_t nod_elt;
 
-	for (; h != NULL; h = h->next)
+	for (nod_elt = 0; h != NULL; h = h->next)
 	{
 		if (h->str == NULL)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
-		h = h->next;
 		nod_elt++;
 	}
 
