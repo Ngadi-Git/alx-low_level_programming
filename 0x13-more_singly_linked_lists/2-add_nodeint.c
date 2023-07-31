b2fe911046d69b629f223b2c1794a282a74eb4e6
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-07-31 21:03:16 +0000

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,12 +2,12 @@
 
 /**
  * add_nodeint - adds a node to the beginning of a linked list
- * @top: pointer to the head of the list
+ * @head: pointer to the head of the list
  * @d: integer to be used as content
  *
  * Return: address of the newly added node
  */
-listint_t *add_nodeint(listint_t **top, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
 
