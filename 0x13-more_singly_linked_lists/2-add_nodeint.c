cf0933d8492aed1b24aed9f34cfcd4108117046c
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-08-01 22:50:51 +0000

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,24 +3,24 @@
 /**
  * add_nodeint - adds a node to the beginning of a linked list
  * @head: pointer to the head of the list
- * @d: integer to be used as content
+ * @n: integer to be used as content
  *
  * Return: address of the newly added node
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *n_node;
+	listint_t *new_node;
 
-	n_node = malloc(sizeof(listint_t));
-	if (n_node != NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node != NULL)
 	{
-		n_node->n = n;
-		n_node->next = *head;
+		new_node->n = n;
+		new_node->next = *head;
 	}
 	else
 		return (NULL);
 	if (*head != NULL)
-		n_node->next = *head;
-	*head = n_node;
-	return (n_node);
+		new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
