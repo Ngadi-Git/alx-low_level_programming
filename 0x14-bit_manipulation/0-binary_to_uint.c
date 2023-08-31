b48a3cb1fb083a02bbb38cd7b2d9cb5ba74a3c0b
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-08-31 21:40:18 +0000

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,33 +6,36 @@
  *
  * Return: The converted unsigned int value.
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num, len, tmp;
+	unsigned int tmp = 0;
+	int ln = 0, base_two = 1;
 
-	num = 0;
-	tmp = 1;
-
-	len = 0;
-
-	if (b == '\0')
+	if (!b)
 		return (0);
 
-	while (b[len] != '\0')
-		len++;
-
-	len--;
-
-	while (len >= 0)
+	while (b[ln] != '\0')
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-
-		num += (b[len] - '0') * tmp;
-		tmp = tmp * 2;
-		len--;
+		ln++;
 	}
 
-	return (num);
+	ln--;
+
+	while (ln >= 0)
+	{
+		if (b[ln] != '0' && b[ln] != '1')
+		{
+			return (0);
+		}
+
+		if (b[ln] & 1)
+		{
+			tmp += base_two;
+		}
+
+		ln--;
+		base_two *= 2;
+	}
+
+	return (tmp);
 }
