6a692a09a226d994af47088041963f7f8290b08d
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-08-31 22:20:51 +0000

    Task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned int bts;
 
-	for (btss = 0; n || m; n >>= 1, m >>= 1)
+	for (bts = 0; n || m; n >>= 1, m >>= 1)
 	{
 		if ((n & 1) != (m & 1))
 			bts++;
