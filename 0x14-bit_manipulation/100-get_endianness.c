4341ceaa7a0e890bb9a31d343fec2fee869cccc9
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-08-31 22:24:24 +0000

    Task 100

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,21 @@
-	
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness.
+ *
+ * Return: If big-endian - 0.
+ *         If little-endian - 1.
+ */
+int get_endianness(void)
+{
+	int digit;
+	char *last;
+
+	digit = 1;
+	last = (char *)&digit;
+
+	if (*last == 1)
+		return (1);
+
+	return (0);
+}	
