1adb6a01e774b598ad743790b9589c9eb21ff26c
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-12 21:19:46 +0000

    All tasks

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,18 +7,19 @@
  *
  * Return: The address of the new element, or NULL on failure.
  */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node;
+	listint_t *newList;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	newList = malloc(sizeof(listint_t));
+	if (newList == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->next = *head;
+	newList->n = n;
+	newList->next = *head;
 
-	*head = new_node;
+	*head = newList;
 
-	return (new_node);
+	return (newList);
 }
