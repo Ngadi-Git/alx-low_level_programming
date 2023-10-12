8a251161ed61de33bf2744d29184206783752d00
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-12 22:54:46 +0000

    all tasks

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Returns the sum of all data (n) in a dlistint_t linked list
+ * @head: Pointer to the head of the linked list
+ * Return: Sum of all data, 0 if the list is empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
