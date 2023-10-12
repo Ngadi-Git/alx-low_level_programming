3167d57400f5481d576473eea896b4c4c73586c4
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-12 22:48:27 +0000

    all tasks

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,24 +1,14 @@
-#include "listsi.h"
+#include "lists.h"
 
 /**
- * listint_len - Returns the number of elements in a linked listint_t list.
- * @h: A pointer to the head of the listint_t list.
+ * dlistint_len - Returns the number of elements in a dlistint_t list
+ * @h: A pointer to the head of the doubly linked list
  *
- * Return: The number of elements in the listint_t list.
- *
+ * Return: The number of elements in the list
  */
-
 size_t dlistint_len(const dlistint_t *h)
 {
-	int count;
-
-	count = 0;
-
-	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
