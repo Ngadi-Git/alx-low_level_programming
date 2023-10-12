fba0e06f4c8d4fcfbf5a909320c316f49a0eed7f
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-10-12 22:35:27 +0000

    all tasks

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,32 +8,16 @@
  * Return: The number of elements in the listint_t list.
  *
  */
-#include "lists.h"
 
-/**
- * dlistint_len - returns the number of elements in
- * a double linked list
- *
- * @h: head of the list
- * Return: the number of nodes
- */
 size_t dlistint_len(const dlistint_t *h)
 {
-	int count;
+	size_t nds = 0;
 
-	count = 0;
-
-	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
+	while (h)
 	{
-		count++;
+		nds++;
 		h = h->next;
 	}
 
-	return (count);
+	return (nds);
 }
