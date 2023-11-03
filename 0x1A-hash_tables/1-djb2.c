0d6cc9b414e864a80db3cbaf319db5dcffef4d4f
Ngadi-Git <ngadi.l.paul@gmail.com>
2023-11-03 23:09:36 +0000

    All tasks

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -8,12 +8,12 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash;
+	unsigned long int hsh;
 	int c;
 
-	hash = 5381;
+	hsh = 5381;
 	while ((c = *str++))
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+		hsh = ((hsh << 5) + hsh) + c; /* hsh * 33 + c */
 
-	return (hash);
+	return (hsh);
 }
